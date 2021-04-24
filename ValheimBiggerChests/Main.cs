d898b10171c24b2c899afe6ee3418af6c8b72315
Franziska Härtner <38294070+fhaertner@users.noreply.github.com>
2021-04-24 20:08:02 +0200

    Added possbility for config file

---
+++ ValheimBiggerChests/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+using System.IO;
+using System.Reflection;
+using BepInEx.Logging;
+using ValheimBiggerChests.Patches;
+
+namespace ValheimBiggerChests
+{
+    public static class PluginInfo
+    {
+        public const string Name = "ValheimBiggerChests";
+        public const string Guid = "Franzi." + Name;
+        public const string Version = "0.0.1";
+    }
+
+
+    [BepInPlugin(PluginInfo.Guid, PluginInfo.Name, PluginInfo.Version)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string
+            MODNAME = PluginInfo.Name,
+            AUTHOR = "Franzi",
+            GUID = AUTHOR + "_" + MODNAME,
+            VERSION = PluginInfo.Version;
+        internal readonly Assembly assembly;
+        internal readonly Harmony harmony;
+        public readonly string modFolder;
+        public Main()
+        {
+            harmony = new Harmony("mod." + PluginInfo.Name);
+            assembly = Assembly.GetExecutingAssembly();
+            modFolder = Path.GetDirectoryName(assembly.Location);
+        }
+        void Awake()
+        {
+            Logger.LogInfo("Loaded " + PluginInfo.Name + " version " + PluginInfo.Version);
+            harmony.PatchAll(assembly);
+            Patches.ContainerPatches.personalChest = (Width: base.Config.Bind<uint>(MODNAME, "Personal Chest Width", Patches.ContainerPatches.personalChest.Width, "Width of personal chest.").Value,
+                Height: base.Config.Bind<uint>(MODNAME, "Personal Chest Height", Patches.ContainerPatches.personalChest.Height, "Height of personal chest.").Value);
+            Patches.ContainerPatches.standardChest = (Width: base.Config.Bind<uint>(MODNAME, "Standard Chest Width", Patches.ContainerPatches.standardChest.Width, "Width of standard chest.").Value,
+                Height: base.Config.Bind<uint>(MODNAME, "Standard Chest Height", Patches.ContainerPatches.standardChest.Height, "Height of standard chest.").Value);
+             Patches.ContainerPatches.reinforcedChest = (Width: base.Config.Bind<uint>(MODNAME, "Reinforced Chest Width", Patches.ContainerPatches.reinforcedChest.Width, "Width of reinforced chest.").Value,
+                Height: base.Config.Bind<uint>(MODNAME, "Reinforced Chest Height", Patches.ContainerPatches.reinforcedChest.Height, "Height of reinforced chest.").Value);
+
+        }
+
+        private void OnDestroy()
+        {
+            harmony.UnpatchAll();
+        }
+    }
+
+}
---
+++ ValheimBiggerChests/Patches/Patches.cs
@@ -0,0 +1,94 @@
+﻿using HarmonyLib;
+
+namespace ValheimBiggerChests.Patches
+{
+    [HarmonyPatch(typeof(Container), "Awake")]
+    public static class ContainerPatches
+    {
+		public static (uint Width, uint Height) personalChest = (3, 2); 
+		public static (uint Width, uint Height) standardChest = (5, 2); 
+		public static (uint Width, uint Height) reinforcedChest = (6, 3); 
+        private static void Prefix(ref Container __instance)
+        {
+            applyContainerSizes(__instance);
+        }
+
+        private static void applyContainerSizes(Container instance)
+        {
+			/*
+            if (instance.m_wagon)
+            {
+                instance.m_width = 8;
+                instance.m_height = 4;
+
+                return;
+            }
+
+            Ship ship = instance.gameObject.transform.parent?.GetComponent<Ship>();
+
+            if (ship)
+            {
+                if (ship.name.StartsWith("VikingShip"))
+                {
+                    instance.m_width = 7;
+                    instance.m_height = 4;
+
+                    return;
+                }
+
+                if (ship.name.StartsWith("Karve"))
+                {
+                    instance.m_width = 3;
+                    instance.m_height = 3;
+
+                    return;
+                }
+
+                return;
+            }*/
+
+            if (instance.name.StartsWith("piece_chest_wood"))
+            {
+				instance.m_width = (int)standardChest.Width;
+                instance.m_height = (int)standardChest.Height;
+
+                return;
+            }
+
+            if (instance.name.StartsWith("piece_chest_private"))
+            {
+                instance.m_width = (int)personalChest.Width;
+                instance.m_height = (int)personalChest.Height;
+				
+                return;
+            }
+
+            if (instance.name.StartsWith("piece_chest"))
+            {
+                instance.m_width = (int)reinforcedChest.Width;
+                instance.m_height = (int)reinforcedChest.Height;
+				
+                return;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Container), "GetHoverText")]
+    public static class DebugHoverText
+    {
+        private static void Postfix(ref Container __instance, ref string __result)
+        {
+            //Ship ship = __instance.gameObject.transform.parent?.GetComponent<Ship>();
+
+            //if (ship)
+            //{
+            //    __result = ship.name;
+            //}
+            //else {
+            //    __result = __instance.name;
+            //}
+        }
+    }
+
+
+}
